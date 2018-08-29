c642fe4fd44072e0808bac34e3f72f5ab30a7f8e
Elta3lab <ahmedfouad2020era@gmail.com>
2018-08-29 14:55:28 +0200

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Raye7"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"   
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success

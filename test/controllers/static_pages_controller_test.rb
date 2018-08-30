ba08d3b420ff8dc2dd55efb9a842849c0aff2776
Elta3lab <ahmedfouad2020era@gmail.com>
2018-08-30 16:39:47 +0200

    add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -27,6 +27,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end

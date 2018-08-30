81231fe6867775b1bf3ebb59ddcaa108fe970e6c
Elta3lab <ahmedfouad2020era@gmail.com>
2018-08-30 19:40:49 +0200

    edit routes for the new created user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'users/new'
+
+  #static pages routes
   root 'static_pages#home'
   get '/help',  to: 'static_pages#help'
   get '/about',to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  
+  #users routes
+  get  '/signup',  to: 'users#new'
   
 end

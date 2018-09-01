db1a843c3a25d1652832a8f1b9bab60e25a4be5f
Elta3lab <ahmedfouad2020era@gmail.com>
2018-09-01 22:11:08 +0200

    finishing reg system from sratch

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
+
+ #the home page for the website (redirect to the user profile unless he's logged out)
   def home
+    redirect_to current_user if current_user.present?
   end
 
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,66 @@
 class UsersController < ApplicationController
-    def new
-   	  @user = User.new
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
+
+  def new
+ 	  @user = User.new
+  end
+
+  def show
+	  @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to Raye7 Web App"
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 
-    def show
-  	  @user = User.find(params[:id])
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        log_in @user
-        flash[:success] = "Welcome to Raye7 Web App"
-        redirect_to @user
-      else
-        render 'new'
-      end
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :phone, :role, :password,
+                                 :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 
-
-
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :phone, :role, :password,
-                                   :password_confirmation)
-    end
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
 end

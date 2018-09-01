663e0821d63a596fb050b8c17d93d09022552ab2
Elta3lab <ahmedfouad2020era@gmail.com>
2018-09-02 01:44:12 +0200

    add seeds, fix testing bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Ahmed Mohamed Fouad",
+             phone: "1273737878",
+             rol: 'Driver',
+             password:              "12345678",
+             password_confirmation: "12345678"
+             )
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,6 +52,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
+    @user.destroy
   end
 
 
